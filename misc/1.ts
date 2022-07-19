9ace50a4b59df57d880abf83f95ef9920b5edfa5
Yusuf Musa <volunux@gmail.com>
2022-07-19 20:28:09 +0100

    First commit

--- misc/1.ts
+++
@@ -1,8 +0,0 @@
-export class EntityCrudFormControllerService<T> extends EntityCrudController<T> {
-
-  public entityForm: FormGroup = new FormGroup({});
-  protected override route: ActivatedRoute;
-
-  constructor(protected eofbs: EntityOneFormBuilderService, protected override service: BaseCrudServiceImpl<T>, protected override router: Router) {
-    super(service, router);
-  } 
